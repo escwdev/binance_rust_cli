260d10ed1af30b0930a2c53c4a58a17d6337d793
Eric <eric.abrahams@e-systemsconsulting.com>
2017-11-14 17:15:21 -0400

    Add basic command line functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+use std::error::Error;
+use std::io::prelude::*;
+use std::env;
+
+pub struct Config {
+    pub query: String,
+}
+
+impl Config {
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a pair query"),
+        };
+
+        Ok(Config{query})
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate binance_rust_cli;
 extern crate reqwest;
 
 #[macro_use]
@@ -5,6 +6,10 @@ extern crate serde_derive;
 extern crate serde_json;
 
 use std::io::Read;
+use std::env;
+use std::process;
+
+use binance_rust_cli::Config;
 
 #[derive(Debug, Deserialize)]
 struct Ticker {
@@ -15,10 +20,12 @@ struct Ticker {
 const TICKER_URL: &'static str = "https://www.binance.com/api/v1/ticker/allPrices";
 
 fn main() {
-    // let client = reqwest::Client::new();
+    let config = binance_rust_cli::Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Probplem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     let mut response = reqwest::get(TICKER_URL)
-        // .send()
         .expect("Failed to send request");
     let mut buf = String::new();
     response.read_to_string(&mut buf).expect("Failed to read response");
